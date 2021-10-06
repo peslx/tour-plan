cedf641d3e0873d7c47819002fb11e42d4706090
peslx <90468331+peslx@users.noreply.github.com>
2021-10-06 08:06:14 +0300

    fixed: minified js and css

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,4 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-
-  // direction: "vertical",
   loop: true,
 
   keyboard: {
@@ -9,7 +6,6 @@ const swiper = new Swiper(".swiper", {
     onlyInViewport: false,
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".slider-button--next",
     prevEl: ".slider-button--prev",
@@ -26,37 +22,6 @@ const reviewsSlider = new Swiper(".reviews-slider", {
 
 $(".parallax-window").parallax({ imageSrc: "img/newsletter.jpg" });
 jQuery(window).trigger("resize").trigger("scroll");
-
-// function send(event, php) {
-//   console.log("Отправка запроса");
-//   event.preventDefault ? event.preventDefault() : (event.returnValue = false);
-//   var req = new XMLHttpRequest();
-//   req.open("POST", php, true);
-//   req.onload = function () {
-//     if (req.status >= 200 && req.status < 400) {
-//       json = JSON.parse(this.response); // Ебанный internet explorer 11
-//       console.log(json);
-
-//       // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
-//       if (json.result == "success") {
-//         // Если сообщение отправлено
-//         alert("Сообщение отправлено");
-//       } else {
-//         // Если произошла ошибка
-//         alert("Ошибка. Сообщение не отправлено");
-//       }
-//       // Если не удалось связаться с php файлом
-//     } else {
-//       alert("Ошибка сервера. Номер: " + req.status);
-//     }
-//   };
-
-//   // Если не удалось отправить запрос. Стоит блок на хостинге
-//   req.onerror = function () {
-//     alert("Ошибка отправки запроса");
-//   };
-//   req.send(new FormData(event.target));
-// }
 
 $(document).ready(function () {
   const burger = $("#burger");
